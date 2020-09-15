351665abc2b81a88fb305292a2cd9ff59d5b9c90
MarkTiukov <tyukov.mark@gmail.com>
2020-09-15 13:46:19 +0300

    * fixed push_back issue * added allocation memory for answer[] * replaced pattern.length() * moved logic from main to a separate class

--- contest1/Task1.cpp
+++ contest1/Task1.cpp
@@ -2,37 +2,64 @@
 #include <string>
 #include <vector>
 
-void countPrefixFunction(std::string& string, std::vector<int>& answer) {
-  ///  Вычисляем префикс-функцию
-  answer.push_back(0);
-  for (int i = 1; i < string.length(); ++i) {
-    int borderLength = answer[i - 1];
-    while (borderLength > 0 && string[i] != string[borderLength]) {
-      borderLength = answer[borderLength - 1];
-    }
-    if (string[borderLength] == string[i]) {
-      ++borderLength;
-    }
-    answer.push_back(borderLength);
+class PatternMatching {
+ private:
+  std::string data;
+  std::string pattern;
+  int dataLength;
+  int patternLength;
+  inline static const std::string DEFAULT_PATTERN_SEPARATOR = "#";
+  std::vector<int> prefixFunction;
+  std::vector<int> patternPositions;
+
+ public:
+
+  PatternMatching() = default;
+  PatternMatching(const std::string& data, const std::string& pattern)
+      : data(data), pattern(pattern), dataLength(data.length()), patternLength(pattern.length()) {
+    prefixFunction.reserve(data.length());
+    countPrefixFunction(this->pattern + DEFAULT_PATTERN_SEPARATOR + this->data, prefixFunction);
   }
-}
+
+  void findPattern() {
+    for (int i = 0; i < dataLength; ++i) {
+      if (prefixFunction[patternLength + i + 1] == patternLength) {
+        // вычитается длина паттерна
+        patternPositions.emplace_back(i - prefixFunction[patternLength + i + 1] + 1);
+      }
+    }
+  }
+
+  void printPatternPositions() {
+    for (auto el: patternPositions) {
+      std::cout << el << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  static void countPrefixFunction(std::string&& string, std::vector<int>& answer) {
+    ///  Вычисляет префикс-функцию
+    answer.emplace_back(0);
+    for (int i = 1; i < string.length(); ++i) {
+      int borderLength = answer[i - 1];
+      while (borderLength > 0 && string[i] != string[borderLength]) {
+        borderLength = answer[borderLength - 1];
+      }
+      if (string[borderLength] == string[i]) {
+        ++borderLength;
+      }
+      answer.emplace_back(borderLength);
+    }
+  }
+
+  virtual ~PatternMatching() = default;
+};
 
 int main() {
   std::string line;
   std::string pattern;
   std::cin >> pattern >> line;
-  std::string united = pattern + "#" + line;
-  std::vector<int> prefixFunction = std::vector<int>();
-  countPrefixFunction(united, prefixFunction);
-  std::vector<int> answer = std::vector<int>();
-  for (int i = 0; i < line.length(); ++i) {
-    if (prefixFunction[pattern.length() + i + 1] == pattern.length()) {
-      // вычитается длина паттерна
-      answer.push_back(i - prefixFunction[pattern.length() + i + 1] + 1);
-    }
-  }
-  for (auto element: answer) {
-    std::cout << element << " ";
-  }
-  std::cout << std::endl;
+  PatternMatching matcher(line, pattern);
+  matcher.findPattern();
+  matcher.printPatternPositions();
 }
