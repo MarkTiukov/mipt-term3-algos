0ae78a90e1549e81b375cbafc7b6247dbf016fa8
MarkTiukov <tyukov.mark@gmail.com>
2020-10-18 20:53:46 +0300

    Initial commit

---
+++ contest2/Task1/Task1.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task1
+//
+//  Created by Марк Тюков on 18.10.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
