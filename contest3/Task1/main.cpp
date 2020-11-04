bc191cf371f54dddabc24318093323d585908fc0
MarkTiukov <tyukov.mark@gmail.com>
2020-11-04 23:36:54 +0300

    Ready for tests

--- contest3/Task1/main.cpp
+++ contest3/Task1/main.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 const double fPRECISION = 1e-6;
 
-struct Point {
+class Point {
+public:
     double x, y, z;
+    Point() = default;
+    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+    Point operator+(const Point &point) const;
+    Point& operator+=(const Point &point);
+    bool operator==(const Point &point) const;
+    bool operator!=(const Point &point) const;
+    Point& operator*=(const double number);
+    Point operator*(const double number) const;
+    Point& operator/=(const double number);
+    Point operator/(const double number) const;
 };
 
-struct Segment {
-    Point a, b;
+class Segment {
+public:
+    Point begin, end;
     
-    Segment(double x1, double y1, double z1, double x2, double y2, double z2) {
-        a = Point();
-        a.x = x1;
-        a.y = y1;
-        a.z = z1;
-        b = Point();
-        b.x = x2;
-        b.y = y2;
-        b.z = z2;
-    }
+    Segment() = default;
+    Segment(Point begin, Point end) : begin(begin), end(end) {}
+
+    double getLengthSquared();
+    double getLength();
+    
 };
 
 double getHeightFromPointOnSegment(const Segment &segment, const Point &point);
@@ -35,68 +44,105 @@ int main() {
     std::cin >> b.x >> b.y >> b.z;
     std::cin >> c.x >> c.y >> c.z;
     std::cin >> d.x >> d.y >> d.z;
-}
-
-bool checkIntersectionOnProjection(const Segment &segment1, const Segment &segment2) {
-    bool result = false;
-    double det = (segment1.b.x-segment1.a.x) * (segment2.a.y - segment2.b.y) -
-    (segment1.b.y - segment1.a.y) * (segment2.a.x - segment2.b.x);
-    double det1 = (segment1.b.x-segment1.a.x) * (segment2.a.y - segment1.a.y) -
-    (segment1.b.y - segment1.a.y) * (segment2.a.x - segment1.a.x);
-    double det2 = (segment2.a.x-segment1.a.x) * (segment2.a.y - segment2.b.y) -
-    (segment2.a.y - segment1.a.y) * (segment2.a.x - segment2.b.x);
-    if (std::abs(det) < fPRECISION) {
-        double part_x = det1 / det;
-        double part_y = det2 / det;
-        if (part_x >= 0 && part_y >=0 && part_x <= 1 && part_y <= 1) {
-            result = true;
-        }
-    }
-    return result;
+    std::cout << std::setprecision(6) << findMinDistanceBetweenTwoSegments(Segment(a, b), Segment(c, d)) << std::endl;
 }
 
 double findMinDistanceBetweenTwoSegments(const Segment &segment1, const Segment &segment2) {
-    if (checkIntersection(segment1, segment2))
-        return 0.0;
-    double result = getHeightFromPointOnSegment(segment1, segment2.a);
-    double newHeight = getHeightFromPointOnSegment(segment1, segment2.b);
-    if (newHeight != -1 && newHeight < result)
-        result = newHeight;
-    newHeight = getHeightFromPointOnSegment(segment2, segment1.a);
-    if (newHeight != -1 && newHeight < result)
-        result = newHeight;
-    newHeight = getHeightFromPointOnSegment(segment2, segment1.b);
-    if (newHeight != -1 && newHeight < result)
-        result = newHeight;
-    if (result == -1) {
-        result = (segment1.a.x - segment2.a.x) * (segment1.a.x - segment2.a.x)
-        + (segment1.a.y - segment2.a.y) * (segment1.a.y - segment2.a.y);
-        newHeight = (segment1.b.x - segment2.b.x) * (segment1.b.x - segment2.b.x)
-        + (segment1.b.y - segment2.b.y) * (segment1.b.y - segment2.b.y);
-        if(newHeight < result)
-            result = newHeight;
-        newHeight = (segment1.b.x - segment2.a.x) * (segment1.b.x - segment2.a.x)
-        + (segment1.b.y - segment2.a.y) * (segment1.b.y - segment2.a.y);
-        if(newHeight < result)
-            result = newHeight;
-        newHeight = (segment1.a.x - segment2.b.x) * (segment1.a.x - segment2.b.x)
-        + (segment1.a.y - segment2.b.y) * (segment1.a.y - segment2.b.y);
-        if(newHeight < result)
-            result = newHeight;
-        result = std::sqrt(result);
+    Point begin = segment1.begin;
+    Point end = segment2.begin;
+    Point new_begin = segment1.begin + Point(1, 0, 0);
+    Point new_end = segment2.begin + Point(1, 0, 0);
+    Point left, right;
+    for (int count = 0; (begin != new_begin || end != new_end) && count < 100; ++count) {
+        begin = new_begin;
+        end   = new_end;
+        left  = segment1.begin;
+        right = segment1.end;
+        while (left != right) {
+            Point a = (left * 2 + right) / 3;
+            Point b = (left + right * 2) / 3;
+            if (Segment(a, end).getLength() <
+                Segment(b, end).getLength()) {
+                right = b;
+            } else {
+                left = a;
+            }
+        }
+        new_begin = left;
+        left  = segment2.begin;
+        right = segment2.end;
+        while (left != right) {
+            auto a = (left * 2 + right) / 3;
+            auto b = (left + right * 2) / 3;
+            if (Segment(new_begin, a).getLength() <
+                Segment(new_begin, b).getLength()) {
+                right = b;
+            } else {
+                left = a;
+            }
+        }
+        
+        new_end = left;
     }
+    
+    return Segment(begin, end).getLength();
+}
+
+Point& Point::operator+=(const Point &point) {
+    this->x += point.x;
+    this->y += point.y;
+    this->z += point.z;
+    return *this;
+}
+
+Point Point::operator+(const Point &point) const {
+    Point result = *this;
+    result += point;
     return result;
 }
 
-double getHeightFromPointOnSegment(const Segment &segment, const Point &point) {
-    return 0.0;
+bool Point::operator==(const Point &point) const {
+    return std::abs(this->x - point.x) < fPRECISION &&
+            std::abs(this->y - point.y) < fPRECISION &&
+            std::abs(this->z - point.z) < fPRECISION;
 }
 
-bool checkIntersection(const Segment &segment1, const Segment &segment2) {
-    return checkIntersectionOnProjection(Segment(segment1.a.x, segment1.a.y, 0, segment1.b.x, segment1.b.y, 0),
-                                                Segment(segment2.a.x, segment2.a.y, 0, segment2.b.x, segment2.b.y, 0)) &&
-                    checkIntersectionOnProjection(Segment(segment1.a.x, 0, segment1.a.z, segment1.b.x, 0, segment1.b.z),
-                                                  Segment(segment2.a.x, 0, segment2.a.z, segment2.b.x, 0, segment2.b.z)) &&
-                    checkIntersectionOnProjection(Segment(0, segment1.a.y, segment1.a.z, 0, segment1.b.y, segment1.b.z),
-                                                  Segment(0, segment2.a.y, segment2.a.z, 0, segment2.b.y, segment2.b.z));
+bool Point::operator!=(const Point &point) const {
+    return !(*this == point);
+}
+
+Point& Point::operator*=(const double number) {
+    this->x *= number;
+    this->y *= number;
+    this->z *= number;
+    return *this;
+}
+
+Point Point::operator*(const double number) const {
+    Point result = *this;
+    result *= number;
+    return result;
+}
+
+Point& Point::operator/=(const double number) {
+    this->x /= number;
+    this->y /= number;
+    this->z /= number;
+    return *this;
+}
+
+Point Point::operator/(const double number) const {
+    Point result = *this;
+    result /= number;
+    return result;
+}
+
+double Segment::getLength() {
+    return std::sqrt(getLengthSquared());
+}
+
+double Segment::getLengthSquared() {
+    return (begin.x - end.x) * (begin.x - end.x) +
+            (begin.y - end.y) * (begin.y - end.y) +
+            (begin.z - end.z) * (begin.z - end.z);
 }
