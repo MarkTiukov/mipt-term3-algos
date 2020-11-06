4c36b7cd6fcf9ed4cfc5dc50e9be4931608f0085
MarkTiukov <tyukov.mark@gmail.com>
2020-11-06 16:45:48 +0300

    Changed sorting

--- contest3/Task2/main.cpp
+++ contest3/Task2/main.cpp
@@ -4,14 +4,17 @@
 #include <algorithm>
 #include <deque>
 #include <iomanip>
+#include <climits>
 
 const double fPRECISION = 1e-9;
 
 struct Point {
     double x, y;
+    double cot;
     
     Point() = default;
     Point(double x, double y) : x(x), y(y) {}
+    
 };
 
 extern std::ostream& operator<<(std::ostream& outputStream, const Point& point);
@@ -21,8 +24,11 @@ bool operator>(const Point& one, const Point& another);
 bool operator==(const Point& one, const Point& another);
 bool operator!=(const Point& one, const Point& another);
 
-void readData(std::vector<Point> &points);
+Point begin;
 
+bool cmp(const Point &a, const Point &b);
+
+void readData(std::vector<Point> &points);
 double getLength(const Point& a, const Point& b);
 bool checkLeftRotation(const Point& a, const Point& b,const Point& c);
 void printMinFence(const std::vector<Point> &points);
@@ -30,6 +36,9 @@ void printMinFence(const std::vector<Point> &points);
 int main() {
     std::vector<Point> points;
     readData(points);
+//    for (auto el : points) {
+//        std::cout << el << std::endl;
+//    }
     printMinFence(points);
 }
 
@@ -41,6 +50,8 @@ void readData(std::vector<Point> &points) {
         std::cin >> points[i].x >> points[i].y;
     }
     std::sort(points.begin(), points.end());
+    begin = points[0];
+    std::sort(++points.begin(), points.end(), cmp);
 }
 
 bool operator==(const Point& one, const Point &other) {
@@ -69,6 +80,8 @@ void printMinFence(const std::vector<Point> &points) {
         ++secondElementPosition;
     fence.push_back(points[secondElementPosition++]);
     for (size_t i = secondElementPosition; i < points.size(); ++i) {
+        if (fence.back() == points[i])
+            continue;
         while (!checkLeftRotation(fence[fence.size() - 2], fence.back(), points[i])) {
             fence.pop_back();
         }
@@ -96,4 +109,14 @@ bool checkLeftRotation(const Point& a, const Point& b,const Point& c) {
 
 double getLength(const Point& a, const Point& b) {
     return std::sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+}
+
+bool cmp(const Point &a, const Point &b) {
+    Point v1(a.x - begin.x, a.y - begin.y);
+    Point v2(b.x - begin.x, b.y - begin.y);
+    double product = v1.y * v2.x - v1.x * v2.y;
+    if (std::abs(product) < fPRECISION) {
+        return a.x < b.x;
+    }
+    return product < 0;
 }
