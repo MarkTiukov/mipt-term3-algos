a29c00c5cc9e2219896b8354d25367326f4fca15
MarkTiukov <tyukov.mark@gmail.com>
2020-11-19 13:01:49 +0300

    Finished Minkovskii addition

--- contest3/Task4/main.cpp
+++ contest3/Task4/main.cpp
@@ -9,29 +9,32 @@ public:
     Point() : x(0), y(0) {}
     Point(long long x, long long y) : x(x), y(y) {}
     
+    Point& operator+=(const Point& other);
+    Point operator+(const Point& other) const;
     Point& operator-=(const Point& other);
     Point operator-(const Point& other) const;
     
 };
 
-struct MyVector {
+struct Vector {
     Point begin;
     Point end;
     long long x, y;
     
-    MyVector() = default;
-    MyVector(const Point& begin, const Point& end) : begin(begin), end(end),
-    x(begin.x - end.x),
-    y(begin.y - end.y) {}
-    MyVector(const Point& radiusVector) : end(radiusVector),
+    Vector() = default;
+    Vector(const Point& begin, const Point& end) : begin(begin), end(end),
+    x(end.x - begin.x),
+    y(end.y - begin.y) {}
+    Vector(const Point& radiusVector) : end(radiusVector),
     x(radiusVector.x),
     y(radiusVector.y) {}
-    MyVector(long long x, long long y) : x(x), y(y), end(Point(x, y)) {}
+    Vector(long long x, long long y) : x(x), y(y), end(Point(x, y)) {}
     
-    static long long crossProduct(const MyVector& v1, const MyVector& v2);
+    static long long crossProduct(const Vector& v1, const Vector& v2);
+    static bool areCollinear(const Vector& v1, const Vector& v2);
     
-    bool operator==(const MyVector& another);
-    bool operator!=(const MyVector& another);
+    bool operator==(const Vector& another) const;
+    bool operator!=(const Vector& another) const;
     
 };
 
@@ -41,6 +44,8 @@ private:
     
     static bool isLowerAndToTheLeft(const Point& p1, const Point& p2);
     static bool compareByAngle(const Point& p1, const Point& p2, const Point& begin);
+    static bool compareByAngle(const Vector& v1, const Vector& v2);
+
     
     struct LowerAndToTheLeftComparator : std::binary_function<Point, Point, bool> {
         Polygon *polygon;
@@ -98,7 +103,7 @@ std::istream& operator>>(std::istream& in, Polygon& polygon) {
 }
 
 Polygon& Polygon::operator+=(const Point& newPoint) {
-    this->points.push_back(newPoint);
+    this->points.emplace_back(newPoint);
     return *this;
 }
 
@@ -110,14 +115,30 @@ Polygon Polygon::operator+(const Point& newPoint) const {
 
 Polygon& Polygon::operator+=(const Polygon& other) {
     // TODO: finish the realization of Minkowski addition
+    size_t n = points.size();
+    size_t m = points.size();
     std::sort(points.begin(), points.end(), LowerAndToTheLeftComparator(this));
     std::sort(++points.begin(), points.end(), AngleComparator(this, points[0]));
     Polygon otherCopy = other;
     std::sort(otherCopy.points.begin(), otherCopy.points.end(), LowerAndToTheLeftComparator(this));
     std::sort(++otherCopy.points.begin(), otherCopy.points.end(), AngleComparator(this, otherCopy.points[0]));
-
     *this += points[0];
     otherCopy += other.points[0];
+    std::vector<Point> result;
+    for (int i = 0, j = 0; i < n && j < m;) {
+        result.emplace_back(points[i] + points[j]);
+        Vector v1(points[i], points[i + 1]), v2(otherCopy.points[j], otherCopy.points[j + 1]);
+        if (Vector::areCollinear(v1, v2)) {
+            ++i;
+            ++j;
+        } else if (compareByAngle(v1, v2)) {
+            ++i;
+        } else {
+            ++j;
+        }
+    }
+    this->points = result;
+    std::cout << points.size() << std::endl;
     return *this;
 }
 
@@ -138,30 +159,40 @@ bool Polygon::isLowerAndToTheLeft(const Point& p1, const Point& p2) {
 }
 
 bool Polygon::compareByAngle(const Point& p1, const Point& p2, const Point& begin) {
-    MyVector v1(p1 - begin);
-    MyVector v2(p2 - begin);
-    long long product = MyVector::crossProduct(v1, v2);
+    Vector v1(p1 - begin);
+    Vector v2(p2 - begin);
+    return compareByAngle(v1, v2);
+}
+
+bool Polygon::compareByAngle(const Vector &v1, const Vector &v2) {
+    long long product = Vector::crossProduct(v1, v2);
     if (product == 0) {
-        if (v1 == MyVector(0, 0))
+        if (v1 == Vector(0, 0))
             return true;
-        if (v2 == MyVector(0, 0))
+        if (v2 == Vector(0, 0))
             return false;
         return v1.x * v1.x + v1.y * v1.y > v2.x * v2.x + v2.y * v2.y;
     }
     return product > 0;
 }
 
-bool MyVector::operator==(const MyVector& another) {
+
+bool Vector::operator==(const Vector& another) const {
     return this->x == another.x && this->y == another.y;
 }
 
-bool MyVector::operator!=(const MyVector& another) {
+bool Vector::operator!=(const Vector& another) const {
     return !(*this == another);
 }
 
-long long MyVector::crossProduct(const MyVector& v1, const MyVector& v2) {
+long long Vector::crossProduct(const Vector& v1, const Vector& v2) {
     return v1.x * v2.y - v1.y * v2.x;
 }
+
+bool Vector::areCollinear(const Vector &v1, const Vector &v2) {
+    return Vector::crossProduct(v1, v2) == 0;
+}
+
 
 Point& Point::operator-=(const Point& other) {
     this->x -= other.x;
@@ -170,6 +201,18 @@ Point& Point::operator-=(const Point& other) {
 }
 
 Point Point::operator-(const Point& other) const {
+    Point result = *this;
+    result -= other;
+    return result;
+}
+
+Point& Point::operator+=(const Point& other) {
+    this->x += other.x;
+    this->y += other.y;
+    return *this;
+}
+
+Point Point::operator+(const Point& other) const {
     Point result = *this;
     result -= other;
     return result;
