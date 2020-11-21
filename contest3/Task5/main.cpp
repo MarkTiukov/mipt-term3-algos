db4820745422d3383b041826a475c7cfa9beb77b
MarkTiukov <tyukov.mark@gmail.com>
2020-11-21 13:30:39 +0300

    Finished finding except for checking for a particular intersection

--- contest3/Task5/main.cpp
+++ contest3/Task5/main.cpp
@@ -28,6 +28,9 @@ public:
     long long getY(long long x) const;
     long long getMin() const;
     long long getMax() const;
+    bool haveItersection(const Segment& other) const;
+    
+    bool operator<(const Segment& other) const;
     
     friend std::istream& operator>>(std::istream& input, Segment& segment);
     friend std::ostream& operator<<(std::ostream& output, const Segment& segment);
@@ -45,10 +48,23 @@ public:
     Event() = delete;
     Event(long long x, EventType type, size_t id) : x(x), type(type), id(id) {}
     
+    size_t getID() const { return id; }
+    EventType getType() const { return type; }
+    
     bool operator<(const Event& other) const;
 };
 
-std::pair<Segment, Segment> findAnyIntersection(const std::vector<Segment>& segments);
+class IntersectionFinder {
+private:
+    std::set<Segment> currentSegments;
+    
+    std::set<Segment>::iterator prev(std::set<Segment>::iterator it);
+    std::set<Segment>::iterator next(std::set<Segment>::iterator it);
+public:
+    IntersectionFinder() = default;
+    std::pair<Segment, Segment> findAnyIntersection(const std::vector<Segment>& segments, bool& hasFound);
+};
+
 
 int main() {
     size_t n;
@@ -57,8 +73,13 @@ int main() {
     for (size_t i = 0; i < n; ++i) {
         std::cin >> segments[i];
     }
-    auto answer = findAnyIntersection(segments);
-    std::cout << answer.first << "\n" << answer.second << std::endl;
+    IntersectionFinder solution;
+    bool intersect;
+    auto answer = solution.findAnyIntersection(segments, intersect);
+    if (intersect)
+        std::cout << "YES\n" << answer.first << "\n" << answer.second << std::endl;
+    else
+        std::cout << "NO" << std::endl;
 }
 
 long long Segment::getY(long long x) const {
@@ -109,15 +130,48 @@ std::ostream& operator<<(std::ostream& output, const Point& point) {
     return output;
 }
 
-std::pair<Segment, Segment> findAnyIntersection(const std::vector<Segment>& segments) {
-    std::set<Segment> currentSegments;
+std::pair<Segment, Segment> IntersectionFinder::findAnyIntersection(const std::vector<Segment>& segments, bool& hasFound) {
+    hasFound = true;
+    std::vector<std::set<Segment>::iterator> segmentsInSet;
     std::vector<Event> events;
     for (size_t i = 0; i < segments.size(); ++i) {
         events.emplace_back(segments[i].getMin(), EventType::ADD, i);
         events.emplace_back(segments[i].getMax(), EventType::REMOVE, i);
     }
     std::sort(events.begin(), events.end());
-    
-    // TODO: finish finding
+    for (auto event : events) {
+        size_t currentID = event.getID();
+        if (event.getType() == EventType::ADD) {
+            std::set<Segment>::iterator nextSegment = currentSegments.lower_bound(segments[currentID]);
+            std::set<Segment>::iterator prevSegment = prev(nextSegment);
+            if (nextSegment != currentSegments.end() && (*nextSegment).haveItersection(segments[currentID]))
+                return std::make_pair(*nextSegment, segments[currentID]);
+            if (prevSegment != currentSegments.end() && (*prevSegment).haveItersection(segments[currentID]))
+                return std::make_pair(*prevSegment, segments[currentID]);
+            segmentsInSet[currentID] = currentSegments.insert(nextSegment, segments[currentID]);
+        }
+    }
+    hasFound = false;
     return std::make_pair(Segment(), Segment());
 }
+
+std::set<Segment>::iterator IntersectionFinder::prev(std::set<Segment>::iterator it) {
+    if (it != currentSegments.begin())
+        return std::prev(it);
+    return currentSegments.end();
+}
+
+std::set<Segment>::iterator IntersectionFinder::next(std::set<Segment>::iterator it) {
+    return std::next(it);
+}
+
+bool Segment::operator<(const Segment& other) const {
+    long long x = std::max(std::min(begin.x, end.x), std::min(other.begin.x, other.end.x));
+    return getY(x) < other.getY(x);
+}
+
+bool Segment::haveItersection(const Segment &other) const {
+//    TODO: complete intersection
+    return true;
+}
+
