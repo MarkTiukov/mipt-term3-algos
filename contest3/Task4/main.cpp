9b48aa8364e67546e38bc641923c89de3c8ee45e
MarkTiukov <tyukov.mark@gmail.com>
2020-11-24 01:01:38 +0300

    Removed a magic number

--- contest3/Task4/main.cpp
+++ contest3/Task4/main.cpp
@@ -7,11 +7,13 @@
 #include "Vector.hpp"
 #include "Polygon.hpp"
 
+static const int OUTPUT_PRECISION = 6;
+
 int main() {
     Polygon A, B;
     std::cin >> A >> B;
     A.sort();
     B.sort();
     Polygon sum = A + B;
-    std::cout << std::fixed << std::setprecision(6) << (sum.getSquare() - A.getSquare() - B.getSquare()) / 2 << std::endl;
+    std::cout << std::fixed << std::setprecision(OUTPUT_PRECISION) << (sum.getSquare() - A.getSquare() - B.getSquare()) / 2 << std::endl;
 }
